76eadc22e546ba30a776475ae26cf8043765bc01
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-09-02 10:36:20 +0700

    praktikum 456

--- src/main/java/com/java/project/praktikum6/denganvariable.java
+++ src/main/java/com/java/project/praktikum6/denganvariable.java
@@ -6,6 +6,7 @@ public class denganvariable {
         for (var value : values){
             total += value;
         }
+        
         int finalValue = total / values.length;
 
         if(finalValue >=75){
